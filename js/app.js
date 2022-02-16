c85a344fba9c16c045222ae4f59a3a9a65e2ea5e
NazmulHasanNahid <nazmulhasan3394@gmail.com>
2022-02-17 00:53:59 +0600

    calculate

---
+++ js/app.js
@@ -0,0 +1,22 @@
+document.getElementById('calculate-button').addEventListener('click' , function(){
+    calculate()
+})
+
+
+function calculate(){
+    const incomeText = document.getElementById('income').value;
+    const income = parseFloat(incomeText)
+    console.log(income);
+    
+    //expense
+    const food = document.getElementById('food').value
+    const rent = document.getElementById('rent').value
+    const clothes  = document.getElementById('clothes').value
+   
+    //calculate
+    const calculateTotal = parseFloat(food) + parseFloat(rent) + parseFloat(clothes)
+    //calculate balance
+    const balance = income - calculateTotal ; 
+    document.getElementById('balance').innerText = balance;
+    document.getElementById('total-expense').innerText =  calculateTotal  ;
+}
